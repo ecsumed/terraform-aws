d6d25818846469825e9e5eb219826069de1922ff
Fahad Saleh <ecsumed@yahoo.com>
2019-03-15 17:41:04 +0500

    Convert send data script to python

--- carbon-graphite/scripts/data-sender.py
+++ carbon-graphite/scripts/data-sender.py
@@ -1,25 +1,28 @@
 #! /usr/bin/python
 
 # Usage:
-# ./data-sender.py 5 2 10 localhost 2003 4
+# ./data-sender.py 5 2 10 4
 
 import sys
 from time import time
 from random import randint
-import subprocess
+import socket
 
 hosts = sys.argv[1:][0]
 metrics = sys.argv[1:][1]
 max_val = sys.argv[1:][2]
-host = sys.argv[1:][3]
-port = sys.argv[1:][4]
-batch = sys.argv[1:][5]
+batch = sys.argv[1:][3]
 
-def nc_send(output, host, port):
-    p1 = subprocess.Popen(["echo", '"{}"'.format(output)], stdout=subprocess.PIPE)
-    p2 = subprocess.Popen(["nc", "-q0", str(host), str(port)], stdin=p1.stdout)
-    p1.stdout.close()
-    p2.communicate()
+host = "127.0.0.1"
+port = 2003
+
+def netcat(host, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.connect(("127.0.0.1", 2003))
+    s.sendall(content)
+    s.shutdown(socket.SHUT_WR)
+    s.close()
 
 epoch = time()
 current_batch = 0
@@ -35,9 +38,10 @@ for host in xrange(1, int(hosts) + 1):
         current_batch += 1
 
         if current_batch >= int(batch):
-            nc_send(payload, host, port)
+            netcat(host, port, payload)
             current_batch = 0
             payload = ""
 
 if payload != "":
-    nc_send(payload, host, port)
+    netcat(host, port, payload)
+
